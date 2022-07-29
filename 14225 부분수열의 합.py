782d685009eed09b97cd9eb8571b651f1ad80cdf
gytjd <hys339631@gmail.com>
2022-07-29 10:54:54 +0900

    20220729

--- 14225 부분수열의 합.py
+++ 14225 부분수열의 합.py
@@ -8,4 +8,24 @@ result=list()
 for i in range(1,n+1):
     result.append(combinations(array,i))
 
-print(result)
+tempA=list()
+for i in result:
+    for j in i:
+        tempA.append(sum(j))
+
+tempA.sort()
+tempA=set(tempA)
+max_temp=max(tempA)
+
+tempB=set()
+for i in range(1,max_temp+1):
+    tempB.add(i)
+
+temp_result=tempB-tempA
+
+if len(temp_result)==0:
+    print(max_temp+1)
+else:
+    print(min(temp_result))
+
+
