27562e6b269fd052ce09e1927dc15ac17676b1ea
gytjd <hys339631@gmail.com>
2022-07-29 15:50:08 +0900

    20220729

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 import sys
-from collections import deque
 
-n,m=map(int,sys.stdin.readline().split())
-graph=list()
-dir=[[-1,0],[1,0],[0,-1],[0,1]]
+n=int(input())
+array=list(map(int,sys.stdin.readline().split()))
+result=list()
 
-for _ in range(n):
-    graph.append(list(sys.stdin.readline().rstrip()))
+def subset(idx):
+    if len(array)==2:
+        result.append(idx)
+        return
 
-alpha=set()
-alpha.add(graph[0][0])
-ans=0
-
-def dfs_recur(x,y,count):
-    global ans
-    ans=max(ans,count)
-
-    for i in range(4):
-        dx=x+dir[i][0]
-        dy=y+dir[i][1]
-
-        if dx>=0 and dx<n and dy>=0 and dy<m and graph[dx][dy] not in alpha:
-            alpha.add(graph[dx][dy])
-            dfs_recur(dx,dy,count+1)
-            alpha.remove(graph[dx][dy])
+    for i in range(1,len(array)-1):
+        a=array[i-1]*array[i+1]
+        temp=array[i]
+        del array[i]
+        subset(idx+a)
+        array.insert(i,temp)
 
 
-dfs_recur(0,0,1)
-print(ans)
+
+subset(0)
+print(result)
+
+
