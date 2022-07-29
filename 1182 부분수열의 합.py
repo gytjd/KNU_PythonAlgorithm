2dfbde4658997b66b6440b15e508be814d9cbfb5
gytjd <hys339631@gmail.com>
2022-07-29 10:18:25 +0900

    20220729

--- 1182 부분수열의 합.py
+++ 1182 부분수열의 합.py
@@ -1,19 +1,18 @@
 import sys
 from itertools import combinations
 
-n,s=map(int,sys.stdin.readline().split())
-array=list(map(int,sys.stdin.readline().split()))
-result=list()
+n, s = map(int, sys.stdin.readline().split())
+array = list(map(int, sys.stdin.readline().split()))
+result = list()
 
-for i in range(1,n+1):
-    result.append((combinations(array,i)))
+for i in range(1, n + 1):
+    result.append((combinations(array, i)))
 
-count=0
-
+count = 0
 
 for i in result:
     for j in i:
-        if sum(j)==s:
-            count+=1
+        if sum(j) == s:
+            count += 1
 
 print(count)
