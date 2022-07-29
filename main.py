78c25198653cb4cf527a5928983c781a920f5151
gytjd <hys339631@gmail.com>
2022-07-29 10:54:54 +0900

    20220729

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import sys
 from itertools import combinations
 
-n,s=map(int,sys.stdin.readline().split())
+n=int(input())
 array=list(map(int,sys.stdin.readline().split()))
 result=list()
 
 for i in range(1,n+1):
-    result.append((combinations(array,i)))
+    result.append(combinations(array,i))
 
-count=0
-
-
+tempA=list()
 for i in result:
     for j in i:
-        if sum(j)==s:
-            count+=1
+        tempA.append(sum(j))
 
-print(count)
+tempA.sort()
+tempA=set(tempA)
+max_temp=max(tempA)
+
+tempB=set()
+for i in range(1,max_temp+1):
+    tempB.add(i)
+
+temp_result=tempB-tempA
+
+if len(temp_result)==0:
+    print(max_temp+1)
+else:
+    print(min(temp_result))
+
+
