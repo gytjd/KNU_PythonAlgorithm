6a7fb46b593355ab46f12752423cb4fbcc5b5c14
gytjd <hys339631@gmail.com>
2022-08-01 14:42:52 +0900

    20220729

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
 import sys
 from collections import deque
 
-n=int(input())
+dir=[[0,1],[1,0],[0,-1],[-1,0]]
 
-mapGraph=[[0 for i in range(n)]for i in range(n)]
+n=int(sys.stdin.readline())
+mapGraph=[list(sys.stdin.readline().rstrip()) for i in range(n)]
+
 visited=[[0 for i in range(n)]for i in range(n)]
-dir=[[-2,-1],[-2,+1],[0,-2],[0,+2],[+2,-1],[+2,+1]]
+RGvisited=[[0 for i in range(n)]for i in range(n)]
 
-r1,c1,r2,c2=map(int,sys.stdin.readline().split())
-queue=deque()
-visited[r1][c1]=1
-queue.append((r1,c1))
-mapGraph[r1][c1]=0
 
-while queue:
-    x,y=queue.popleft()
+def BFS_iter(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        dx,dy=queue.popleft()
 
-    if x==r2 and y==c2:
-        print(mapGraph[x][y])
-        break
+        for i in range(4):
+            nx=dx+dir[i][0]
+            ny=dy+dir[i][1]
 
-    for i in range(6):
-        dx=x+dir[i][0]
-        dy=y+dir[i][1]
+            if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                if mapGraph[dx][dy] == "R" or mapGraph[dx][dy] == "G":
+                    if visited[nx][ny] == 0 and (mapGraph[nx][ny] == "R" or mapGraph[nx][ny] == "G"):
+                        visited[nx][ny] = 1
+                        queue.append((nx, ny))
 
-        if dx<0 or dx>=n or dy<0 or dy>=n:
-            continue
+                elif mapGraph[dx][dy] == "B":
+                    if visited[nx][ny] == 0 and mapGraph[nx][ny] == "B":
+                        visited[nx][ny] = 1
+                        queue.append((nx, ny))
+    return 1
 
-        if visited[dx][dy]==0:
-            queue.append((dx,dy))
-            visited[dx][dy]=1
-            mapGraph[dx][dy]=mapGraph[x][y]+1
-else:
-    print(-1)
+
+def BFS_normal(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        dx,dy=queue.popleft()
+
+        for i in range(4):
+            nx=dx+dir[i][0]
+            ny=dy+dir[i][1]
+
+            if nx>=0 and nx<n and ny>=0 and ny<n:
+                if mapGraph[dx][dy] == mapGraph[nx][ny]:
+                    if RGvisited[nx][ny] == 0:
+                        RGvisited[nx][ny] = 1
+                        queue.append((nx, ny))
+    return 1
+
+rgcount=0
+count=0
+for i in range(n):
+    for j in range(n):
+        if visited[i][j]==0 and BFS_iter(i,j)==1:
+            visited[i][j]=1
+            count+=1
+        if RGvisited[i][j]==0 and BFS_normal(i,j)==1:
+            RGvisited[i][j]=1
+            rgcount+=1
+
+print(rgcount,count)
