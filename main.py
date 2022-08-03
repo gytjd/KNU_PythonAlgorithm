7339157e913c614886e4d969bf2313c2180f0734
gytjd <hys339631@gmail.com>
2022-08-03 14:20:01 +0900

    20220803

--- main.py
+++ main.py
@@ -1,20 +1,59 @@
 import sys
+from collections import deque
 
-n=int(input())
-conf=[[0]*2 for i in range(n)]
+n,m=map(int,sys.stdin.readline().split())
+graph=[list(map(int,sys.stdin.readline().split()))for i in range(m)]
+dir=[[0,1],[1,0],[0,-1],[-1,0]]
 
-for i in range(n):
-    start,end=map(int,sys.stdin.readline().split())
-    conf[i][0]=start
-    conf[i][1]=end
+queue=deque()
+tempQueue=deque()
 
-conf.sort(key=lambda x:(x[1],x[0]))
+for i in range(m):
+    for j in range(n):
+        if graph[i][j]==1:
+            queue.append((i,j))
 
-end=conf[0][1]
-count=1
-for i in range(1,n):
-    if conf[i][0]>=end:
-        count+=1
-        end=conf[i][1]
 
-print(count)
+def is_eqaul():
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j]==0:
+                return 0
+    return 1
+
+
+
+def BFS_iter():
+
+    global queue
+    global tempQueue
+
+    tempQueue=queue
+    queue=deque()
+
+    while tempQueue:
+        x,y=tempQueue.popleft()
+
+        for i in range(4):
+            dx=x+dir[i][0]
+            dy=y+dir[i][1]
+
+            if dx<0 or dx>=m or dy<0 or dy>=n:
+                continue
+
+            if graph[dx][dy]==-1:
+                continue
+
+            if graph[dx][dy]==0:
+                graph[dx][dy]=1
+                queue.append((dx,dy))
+
+count=0
+while queue:
+    BFS_iter()
+    count+=1
+
+if is_eqaul():
+    print(count-1)
+else:
+    print(-1)
